89e14ec9a53615f217b6d161178320f3ab10ec03
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-28 22:42:43 +0300

    Create Modal.jsx
    Add a minimal markup template

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,11 @@
+// import PropTypes from 'prop-types';
+
+export const Modal = () => {
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
