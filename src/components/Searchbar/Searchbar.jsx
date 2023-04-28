6faf84dc79428d13267b45e8059dbe1bd7b712ae
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-28 22:42:13 +0300

    Create Searchbar.jsx
    Add a minimal markup template

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,25 @@
+// import PropTypes from 'prop-types';
+import { Formik, Form, Field } from 'formik';
+// import * as yup from 'yup';
+
+export const Searchbar = () => {
+  return (
+    <header className="searchbar">
+      <Formik>
+        <Form className="form">
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
+
+          <Field
+            className="input"
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </Form>
+      </Formik>
+    </header>
+  );
+};
