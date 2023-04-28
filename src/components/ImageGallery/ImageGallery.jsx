c620107fa4e080a60c678a88321b6607f374b865
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-28 22:42:23 +0300

    Create ImageGallery.jsx
    Add a minimal markup template

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,10 @@
+// import PropTypes from 'prop-types';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+
+export const ImageGallery = () => {
+  return (
+    <ul className="gallery">
+      <ImageGalleryItem />
+    </ul>
+  );
+};
