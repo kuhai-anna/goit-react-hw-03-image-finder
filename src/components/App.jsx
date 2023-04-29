051a67e70b4bc6aefe8c3c13f3aa20b90b480a03
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-29 21:50:38 +0300

    Add section component to the App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,17 @@ import { Button } from './Button/Button';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
 import { Searchbar } from './Searchbar/Searchbar';
+import { Section } from './Section/Section';
 
 export class App extends Component {
   render() {
     return (
       <>
         <Searchbar />
-        <ImageGallery />
-        <Button />
+        <Section>
+          <ImageGallery />
+          <Button />
+        </Section>
         <Loader />
       </>
     );
