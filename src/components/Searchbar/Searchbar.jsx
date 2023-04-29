4bc8f9708f8c2d058aaf3abbb2df95d5d13eaef1
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-29 21:51:29 +0300

    Update Field component in Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -14,8 +14,8 @@ export const Searchbar = () => {
           <Field
             className="input"
             type="text"
-            autocomplete="off"
-            autofocus
+            autoComplete="off"
+            autoFocus
             placeholder="Search images and photos"
           />
         </Form>
