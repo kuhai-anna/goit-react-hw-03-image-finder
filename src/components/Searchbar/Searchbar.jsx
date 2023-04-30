8dfd466894260442c55674da0b152b1a6966e501
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-30 03:42:55 +0300

    Add IconButton component to the Searchbar
    Add icon to this button. Add IconContext for search button

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,15 +1,25 @@
 // import PropTypes from 'prop-types';
+import { BsSearch } from 'react-icons/bs';
+import { IconContext } from 'react-icons';
 import { Formik, Form, Field } from 'formik';
 // import * as yup from 'yup';
+import { IconButton } from 'components/IconButton/IconButton';
 
 export const Searchbar = () => {
   return (
     <header className="searchbar">
       <Formik>
         <Form className="form">
-          <button type="submit" className="button">
-            <span className="button-label">Search</span>
-          </button>
+          <IconContext.Provider
+            value={{
+              color: '#212121',
+              size: '20px',
+            }}
+          >
+            <IconButton type="submit" aria-label="Search button">
+              <BsSearch width="40" height="40" />
+            </IconButton>
+          </IconContext.Provider>
 
           <Field
             className="input"
