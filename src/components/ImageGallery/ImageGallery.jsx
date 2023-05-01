ee44fa162189c207456b9c06f5d98a041fc3a076
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-01 19:21:24 +0300

    Update ImageGallery.jsx
    Add params from the query to render the ImageGallery markup

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,10 +1,18 @@
 import PropTypes from 'prop-types';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 
-export const ImageGallery = ({ children, onClick }) => {
+export const ImageGallery = ({ images, children, onClick }) => {
   return (
     <ul className="gallery">
-      <ImageGalleryItem onClick={onClick} />
+      {images.map(({ id, webformatURL, largeImageURL, tags }) => (
+        <ImageGalleryItem
+          key={id}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          tags={tags}
+          onClick={onClick}
+        />
+      ))}
       {children}
     </ul>
   );
