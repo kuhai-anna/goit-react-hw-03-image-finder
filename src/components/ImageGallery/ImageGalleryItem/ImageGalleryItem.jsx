4c759df60b54fbc3815e6d7144723e05638a2e43
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-01 19:21:46 +0300

    Update ImageGalleryItem.jsx
    Add params from the query to render the ImageGalleryItem markup

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,9 +1,18 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-export const ImageGalleryItem = () => {
+export const ImageGalleryItem = ({
+  webformatURL,
+  largeImageURL,
+  tags,
+  onClick,
+}) => {
   return (
     <li className="gallery-item">
-      <img src="" alt="" />
+      <img src={webformatURL} alt={tags} onClick={onClick} />
     </li>
   );
+};
+
+ImageGalleryItem.propTypes = {
+  onClick: PropTypes.func.isRequired,
 };
