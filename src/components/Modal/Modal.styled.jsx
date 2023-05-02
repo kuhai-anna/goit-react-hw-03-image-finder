56d4aee1e61b6ac80b4b3dc304d4748cfb883b02
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-03 01:57:54 +0300

    Change styles for the Modal component

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -17,8 +17,8 @@ export const ModalContent = styled.div`
   min-height: 300px;
   max-width: 600px;
   width: 100%;
-  padding: ${props => props.theme.spacing(3)};
-  background-color: #ffffff;
+  /* padding: ${props => props.theme.spacing(3)}; */
+  /* background-color: #ffffff; */
   border-radius: 4px;
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2) 0px, 1px, 1px,
     0px rgba(0, 0, 0, 0.4), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
