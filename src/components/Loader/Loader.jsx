33344764b34edeed2ad2f999ddc9c86ff0447b5b
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-02 21:15:38 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,24 @@
-// import PropTypes from 'prop-types';
+import { ThreeDots } from 'react-loader-spinner';
 
-export const Loader = () => {};
+export const Loader = () => {
+  const loaderParams = {
+    color: '#c2014ef4',
+    // color: '#0171c2f4',
+    ariaLabel: 'three-dots-loading',
+  };
+
+  const loaderWrapperStyle = {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100vw',
+    height: '100vh',
+    // backgroundColor: '#d9effff4',
+  };
+  return (
+    <ThreeDots
+      {...loaderParams}
+      wrapperStyle={loaderWrapperStyle}
+      visible={true}
+    />
+  );
+};
