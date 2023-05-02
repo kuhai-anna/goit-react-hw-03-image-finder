06107be955b122605f6b362969b3ed3d837d1896
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-03 00:12:38 +0300

    Update App.jsx
    1. Remove comments
    2. Add viewLoadMoreBtn func
    3. Add onSelectImage func
    4. Add render Button component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,17 @@
 import { Component } from 'react';
-// import { ThreeDots } from 'react-loader-spinner';
-// import { fetchImagesWithQuery } from '../services/image-api';
-// import { Button } from './Button/Button';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-// import { Loader } from './Loader/Loader';
 import { Searchbar } from './Searchbar/Searchbar';
 import { Section } from './Section/Section';
 import { Modal } from './Modal/Modal';
+import { Button } from './Button/Button';
 
 export class App extends Component {
   state = {
     searchQuery: '',
-    // images: [],
-    // totalHits: null,
-    // isLoading: false,
-    // error: null,
+    totalHits: null,
     selectedImg: null,
     showModal: false,
   };
-
-  // async componentDidMount() {
-  //   this.setState({ isLoading: true });
-
-  //   try {
-  //     const { hits, totalHits } = await fetchImagesWithQuery('cat');
-
-  //     this.setState({ images: hits, totalHits });
-  //     console.log(this.state.images);
-
-  //     if (hits.length === 0) {
-  //       throw new Error();
-  //     }
-  //   } catch (error) {
-  //     this.setState({ error });
-  //   } finally {
-  //     this.setState({ isLoading: false });
-  //   }
-  // }
 
   handleFormSubmit = searchQuery => {
     this.setState({ ...searchQuery });
@@ -44,61 +19,30 @@ export class App extends Component {
 
   // передача посилання на активну картинку
   selectedImg = largeImageURL => {
-    // const { images } = this.state;
-    // const imageURL = images.map(image => image.largeImageURL);
-    console.log(largeImageURL);
     this.setState({ selectedImg: largeImageURL });
+  };
+
+  viewLoadMoreBtn = totalHits => {
+    this.setState({ totalHits });
   };
 
   // відкриття та закриття модалки
   toggleModal = () => {
-    // this.setState({ isLoading: true });
     this.setState(({ showModal }) => ({ showModal: !showModal }));
-    // this.setState({ isLoading: false });
   };
 
   render() {
-    const {
-      searchQuery,
-      // images,
-      // totalHits,
-      // isLoading,
-      // error,
-      showModal,
-      selectedImg,
-    } = this.state;
-
-    // const loaderParams = {
-    //   color: '#c2014ef4',
-    //   // color: '#0171c2f4',
-    //   ariaLabel: 'three-dots-loading',
-    // };
-
-    // const loaderWrapperStyle = {
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   width: '100vw',
-    //   height: '100vh',
-    //   // backgroundColor: '#d9effff4',
-    // };
+    const { searchQuery, totalHits, showModal, selectedImg } = this.state;
 
     return (
       <>
         <Searchbar onSubmit={this.handleFormSubmit} />
-        {/* {error && <p>Whoops, something went wrong: {error.message}</p>} */}
-        {/* {isLoading && (
-          <ThreeDots
-            {...loaderParams}
-            wrapperStyle={loaderWrapperStyle}
-            visible={true}
-          />
-        )} */}
-        {/* {images.length > 0 && ( */}
         <Section>
           <ImageGallery
             searchQuery={searchQuery}
-            // images={images}
             onClick={this.toggleModal}
+            onSelectImage={this.selectedImg}
+            viewLoadMoreBtn={this.viewLoadMoreBtn}
           >
             {showModal && (
               <Modal onClose={this.toggleModal}>
@@ -106,10 +50,8 @@ export class App extends Component {
               </Modal>
             )}
           </ImageGallery>
-          {/* {totalHits > 12 && <Button />} */}
+          {totalHits > 12 && <Button />}
         </Section>
-        {/* )} */}
-        {/* <Loader /> */}
       </>
     );
   }
