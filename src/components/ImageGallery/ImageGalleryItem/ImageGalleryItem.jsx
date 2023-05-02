bedb46870ac7b6fa0b21479cba3cfcc15fe3e642
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-02 23:50:23 +0300

    Update ImageGalleryItem.jsx
    1. Add PropTypes
    2. Add handleImageClick func that passes the largeImageURL to the App component

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -5,14 +5,24 @@ export const ImageGalleryItem = ({
   largeImageURL,
   tags,
   onClick,
+  onSelectImage,
 }) => {
+  const handleImageClick = () => {
+    onClick();
+    onSelectImage(largeImageURL);
+  };
+
   return (
     <li className="gallery-item">
-      <img src={webformatURL} alt={tags} onClick={onClick} />
+      <img src={webformatURL} alt={tags} onClick={handleImageClick} />
     </li>
   );
 };
 
 ImageGalleryItem.propTypes = {
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  onSelectImage: PropTypes.func.isRequired,
 };
