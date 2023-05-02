9f9076777960b10c31cc6e64f6770c8b35ab0a85
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-03 01:56:46 +0300

    Update image-api.js
    Change the page number

--- src/services/image-api.js
+++ src/services/image-api.js
@@ -3,19 +3,18 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '34648094-ca1417e5f0eb28e7a5cc77423';
 
-export const fetchImagesWithQuery = async searchQuery => {
+export const fetchImagesWithQuery = async (searchQuery, page) => {
   const searchParams = new URLSearchParams({
     key: `${KEY}`,
     q: `${searchQuery}`,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: `true`,
-    page: '1',
+    page: `${page}`,
     per_page: '12',
   });
 
   const { data } = await axios.get(`${BASE_URL}?${searchParams}`);
-  // this.incrementPage();
 
   return data;
 };
