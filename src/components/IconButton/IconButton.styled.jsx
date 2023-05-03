907c84d4237079789daeee046c5349c48317e0aa
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-03 22:50:50 +0300

    Update IconButton.styled.jsx

--- src/components/IconButton/IconButton.styled.jsx
+++ src/components/IconButton/IconButton.styled.jsx
@@ -4,24 +4,22 @@ export const Button = styled.button`
   margin: 0 auto;
   padding: ${props => props.theme.spacing(1)} ${props => props.theme.spacing(3)};
 
+  width: 48px;
+  height: 36px;
   outline: none;
-  border: 1px solid transparent;
+  border: none;
   border-radius: ${props => props.theme.spacing(1)};
-  background-color: ${props => props.theme.colors.btn};
-  background-color: #e1e1e1;
+  background-color: ${props => props.theme.colors.bgdBtn};
 
   color: ${props => props.theme.colors.icons};
 
-  transition-property: background-color, border-color, color;
+  transition-property: background-color, color;
   transition-duration: 250ms;
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
 
   :hover,
   :focus {
-    /* background-color: ${props => props.theme.colors.btnHover}; */
-    background-color: #c1c1c1;
-    border-color: ${props => props.theme.colors.borderHover};
+    background-color: ${props => props.theme.colors.btnHover};
     color: ${props => props.theme.colors.iconsHover};
-    color: #f7f7f7;
   }
 `;
