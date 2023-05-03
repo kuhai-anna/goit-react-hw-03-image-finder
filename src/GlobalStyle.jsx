c7a8a04aebcaecc1e4541b7776e544ab7d5080bd
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-03 18:10:35 +0300

    Add styles to the GlobalStyle component

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+  width: 100vw;
+  overflow-x: hidden;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
 body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
